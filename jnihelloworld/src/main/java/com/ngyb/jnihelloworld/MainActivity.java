e00a6657891da7a2c514051777b92e4653ef8906
南宫燚滨 <nangongyibin@gmail.com>
2020-07-11 15:04:33 +0800

    不使用Androidx

--- jnihelloworld/src/main/java/com/ngyb/jnihelloworld/MainActivity.java
+++ jnihelloworld/src/main/java/com/ngyb/jnihelloworld/MainActivity.java
@@ -1,10 +1,9 @@
 package com.ngyb.jnihelloworld;
 
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Toast;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
